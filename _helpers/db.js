c8dd0e3436e37987217aa3fd94b1a96623cd2521
ammarGanja <ammar.cmarix@gmail.com>
2022-01-25 16:56:06 +0530

    backend

---
+++ _helpers/db.js
@@ -0,0 +1,15 @@
+const config = require("config.json");
+const mongoose = require("mongoose");
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(
+  process.env.MONGODB_URI || config.connectionString,
+  connectionOptions
+);
+mongoose.Promise = global.Promise;
+
+module.exports = {
+  User: require("../users/user.model"),
+};
---
+++ server.js
@@ -0,0 +1,81 @@
+require("rootpath")();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("_helpers/jwt");
+const errorHandler = require("_helpers/error-handler");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.1",
+    info: {
+      title: "My app apis in swaager",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./users/user.controller.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ *
+ */
+app.get("/books", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: "Harry Potter",
+    },
+  ]);
+});
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use("/users", require("./users/user.controller"));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+const server = app.listen(port, function () {
+  console.log("Server listening on port " + port);
+});
